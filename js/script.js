9c62ccbdd73386cfac54ccaf2708577956c8f2ec
Guilherme Lopes Antunes <108500253+GuilhermeLopesAntunes@users.noreply.github.com>
2023-07-28 18:23:34 -0300

    Jogo pronto

--- js/script.js
+++ js/script.js
@@ -1,121 +1,176 @@
-const canvas = document.querySelector("canvas")//Selecionar elementos do html
-const ctx = canvas.getContext("2d") // obtem contexto de desenho
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const score = document.querySelector(".score--value");
+const finalScore = document.querySelector(".final-core > span");
+const menu = document.querySelector(".menuscreen");
+const buttonPlay = document.querySelector(".btn-play");
+const audio = new Audio("../assets/audio.mp3");
 
-const h1 =document.querySelector("h1")
-const size = 30
+const size = 30;
+const initialPosition = { x: 270, y: 240 };
+let snake = [initialPosition];
+let direction, loopId;
 
-const snake = [
-    {x:0,y:0}
-] //Criando o array da cobra
+const incrementScore = () => {
+  score.innerText = parseInt(score.innerText) + 10;
+};
 
-const randomNumber=(min,max)=>{
-    return Math.round(Math.random()*(max-min) +min)
-}
-const randomPosition=()=>{
-    const number = randomNumber(0,canvas.width-size)
-    return Math.round(number/30) *30
-}
-h1.innerText = randomPosition()
+const randomNumber = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+const randomPosition = () => {
+  const number = randomNumber(0, canvas.width - size);
+  return Math.round(number / 30) * 30;
+};
+
+const randomColor = () => {
+  const red = randomNumber(0, 255);
+  const green = randomNumber(0, 255);
+  const blue = randomNumber(0, 255);
+  return `rgb(${red},${green},${blue})`;
+};
+
 const food = {
-    x:randomPosition(),
-    y:randomPosition(),
-    color: "yellow"
-}
+  x: randomPosition(),
+  y: randomPosition(),
+  color: randomColor(),
+};
 
-const drawFood = ()=>{
+const drawFood = () => {
+  const { x, y, color } = food;
+  ctx.shadowColor = color;
+  ctx.shadowBlur = 6;
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, size, size);
+  ctx.shadowBlur = 0;
+};
 
-    const {x,y,color} = food
-
-    ctx.shadowColor = color
-    ctx.shadowBlur = 6
-    ctx.fillStyle = color
-    ctx.fillRect(x,y,size,size)
-    ctx.shadowBlur=0
-}
 const drawSnake = () => {
-    ctx.fillStyle = "#ddd"
-    
-    snake.forEach((position,index)=>{
-
-        if(index==snake.length -1){
-            ctx.fillStyle = "white"
-        }
-        ctx.fillRect(position.x,position.y,size,size)
-    })
-}
-
-let direction,loopId
-
-const moveSnake = () =>{
-if(!direction) return
-    const head = snake[snake.length-1] //Pega o ultimo elemento do array
-
-    snake.shift() //remove o primeito elemento do array
-    
-    if(direction=="right"){
-        snake.push({x:head.x + size,y:head.y})
+  ctx.fillStyle = "#ddd";
+  snake.forEach((position, index) => {
+    if (index == snake.length - 1) {
+      ctx.fillStyle = "white";
     }
-    if(direction=="left"){
-        snake.push({x:head.x - size,y:head.y})
+    ctx.fillRect(position.x, position.y, size, size);
+  });
+};
+
+const moveSnake = () => {
+  if (!direction) return;
+  const head = snake[snake.length - 1];
+
+  snake.shift();
+
+  if (direction == "right") {
+    snake.push({ x: head.x + size, y: head.y });
+  }
+  if (direction == "left") {
+    snake.push({ x: head.x - size, y: head.y });
+  }
+  if (direction == "down") {
+    snake.push({ x: head.x, y: head.y + size });
+  }
+  if (direction == "up") {
+    snake.push({ x: head.x, y: head.y - size });
+  }
+};
+
+const drawGrid = () => {
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#191919";
+
+  for (let i = 30; i < canvas.width; i += 30) {
+    ctx.beginPath();
+    ctx.lineTo(i, 0);
+    ctx.lineTo(i, 600);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.lineTo(0, i);
+    ctx.lineTo(600, i);
+    ctx.stroke();
+  }
+};
+
+const checkIt = () => {
+  const head = snake[snake.length - 1];
+
+  if (head.x == food.x && head.y == food.y) {
+    incrementScore();
+    snake.push(head);
+    audio.play();
+
+    let x = randomPosition();
+    let y = randomPosition();
+
+    while (snake.find((position) => position.x == x && position.y == y)) {
+      x = randomPosition();
+      y = randomPosition();
     }
-    if(direction=="down"){
-        snake.push({x:head.x,y:head.y + size})
-    }
-    if(direction=="up"){
-        snake.push({x:head.x,y:head.y - size})
-    }
-}
+    food.x = x;
+    food.y = y;
+    food.color = randomColor();
+  }
+};
 
-const drawGrid = ()=>{
-    ctx.lineWidth = 1
-    ctx.strokeStyle = "#191919"
+const checkCollision = () => {
+  const head = snake[snake.length - 1];
+  const canvasLimit = canvas.width - size;
+  const neckIndex = snake.length - 2;
+  const wallCollision =
+    head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit;
+  const selfCollision = snake.find((position, index) => {
+    return (
+      index < neckIndex && position.x == head.x && position.y == head.y
+    );
+  });
 
-    for(let i=30;i<canvas.width;i+=30){
-        ctx.beginPath()
-        ctx.lineTo(i,0)
-        ctx.lineTo(i,600)
+  if (wallCollision || selfCollision) {
+    gameOver();
+  }
+};
 
-        ctx.stroke()
-        
-        ctx.beginPath()
-        ctx.lineTo(0,i)
-        ctx.lineTo(600,i)
+const gameOver = () => {
+  direction = undefined;
+  menu.style.display = "flex";
+  finalScore.innerText = score.innerText;
+  canvas.style.filter = "blur(2px)";
+};
 
-        ctx.stroke()
-    }
-    
-}
+const gameLoop = () => {
+  clearInterval(loopId);
+  ctx.clearRect(0, 0, 600, 600);
+  drawGrid();
+  drawFood();
 
-const gameLoop = ()=> {
-    clearInterval(loopId)
-    ctx.clearRect(0,0,600,600)
-    drawGrid()
-    drawFood()
+  moveSnake();
+  drawSnake();
+  checkIt();
+  checkCollision();
 
-    moveSnake()
-    drawSnake()
+  loopId = setTimeout(gameLoop, 300);
+};
 
-    loopId=setTimeout(() => {
-        gameLoop()
-    }, 300);
-}
+gameLoop();
 
+document.addEventListener("keydown", ({ key }) => {
+  if (key == "ArrowRight" && direction != "left") {
+    direction = "right";
+  } else if (key == "ArrowLeft" && direction != "right") {
+    direction = "left";
+  } else if (key == "ArrowDown" && direction != "up") {
+    direction = "down";
+  } else if (key == "ArrowUp" && direction != "down") {
+    direction = "up";
+  }
+});
 
-gameLoop()
-
-document.addEventListener('keydown',({key}) =>{
-    if(key =="ArrowRight" && direction != "left"){
-        direction = "right"
-    }
-    if(key =="ArrowLeft" && direction != "right"){
-        direction = "left"
-    }
-    if(key =="ArrowDown" && direction != "up"){
-        direction = "down"
-    }
-}) /*Serve para pegar qual botão o usuário apertou*/
-/*
-ctx.fillStyle="red" // Estilo de preenchimento
-ctx.fillRect(300, 300, 50, 50)//desenha um quadrado preenchido
-*/
-
+buttonPlay.addEventListener("click", () => {
+  score.innerText = "00";
+  menu.style.display = "none";
+  canvas.style.filter = "none";
+  snake = [initialPosition];
+  direction = undefined; 
+  gameLoop();
+});
